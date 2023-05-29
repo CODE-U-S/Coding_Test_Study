51b22f7b0572e06c595b0ffef54d50b001cc7544
Jinseon <128007743+dlwlstjs@users.noreply.github.com>
2023-05-29 22:07:04 +0900

    Create 킹, 퀸, 룩, 비숍, 나이트, 폰 .java
    https://www.acmicpc.net/problem/3003

---
+++ Jinseon/킹, 퀸, 룩, 비숍, 나이트, 폰 .java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int king = 1, queen = 1, rook = 2, bishop = 2, knight = 2, pawn = 8;
+		Scanner sc = new Scanner(System.in);
+		
+		int lose = sc.nextInt();
+		king = king - lose;
+		
+		lose = sc.nextInt();
+		queen = queen - lose;
+		
+		lose = sc.nextInt();
+		rook = rook - lose;
+		
+		lose = sc.nextInt();
+		bishop = bishop - lose;
+		
+		lose = sc.nextInt();
+		knight = knight - lose;
+		
+		lose = sc.nextInt();
+		pawn = pawn - lose;
+		
+		System.out.println(king + " " + queen + " " + rook + " " + bishop + " " + knight + " " + pawn);
+	}
+}
