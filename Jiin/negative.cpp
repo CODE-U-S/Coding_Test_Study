622f4d1754af72cdda4d721f4837e5c1d3906f97
darecoco <66424222+darecoco@users.noreply.github.com>
2023-06-11 23:41:27 +0900

    [PGS] 첫 번째로 나오는 음수 / Lv. 0
    https://school.programmers.co.kr/learn/courses/30/lessons/181896?language=cpp

---
+++ Jiin/negative.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = -1;
+    for(int i = 0; i < num_list.size(); i++){
+        if(num_list[i] < 0){
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
