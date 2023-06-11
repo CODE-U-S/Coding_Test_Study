0364e4c551478b4ed684ff6c4d366c64757784f3
phinsso <naso01247@gmail.com>
2023-06-11 16:20:07 +0900

    [BOJ] 모음의 개수 / 브론즈 4
    https://www.acmicpc.net/problem/1264

---
+++ Sojeong/Baekjoon/BOJ_1264.java
@@ -0,0 +1,24 @@
+package Sojeong.Baekjoon;
+
+public class BOJ_1264 {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			String input = br.readLine();
+			String str = input.toLowerCase();
+			int cnt = 0;
+			
+			if(str.equals("#")) break;
+			
+			for(int i = 0; i < str.length(); i++) {
+				if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+					cnt++;
+				}
+			}
+			System.out.println(cnt);
+		}
+		br.close();
+
+	}
+}
