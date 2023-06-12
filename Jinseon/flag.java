b30f7ba0897ac0e9501d423bb2aec5a7aa624245
Jinseon <128007743+dlwlstjs@users.noreply.github.com>
2023-06-12 09:16:14 +0900

    [PGS] flag에 따라 다른 값 반환하기 / Level 0
    https://school.programmers.co.kr/learn/courses/30/lessons/181933

--- Jinseon/flag.java
+++ Jinseon/flag.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int a, int b, boolean flag) {
         int answer = 0;
-        if(flag){
-            answer = a + b;
-        } else {
-            answer = a - b;
-        }
-        return answer;
+        return (flag) ? a + b : a - b;
     }
 }
