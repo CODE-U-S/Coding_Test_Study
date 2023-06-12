f1b5e537df682ecfc8b9da792f8b87b0711323e1
Jinseon <128007743+dlwlstjs@users.noreply.github.com>
2023-06-12 09:15:05 +0900

    [PGS] 주사위 게임3 / Level 0
    https://school.programmers.co.kr/learn/courses/30/lessons/181916

--- Jinseon/dice.java
+++ Jinseon/dice.java
@@ -1,12 +1,20 @@
-class Solution {
-    public int solution(int a, int b, int c, int d) {
-		int ran[] = new int[4];
-        ran[0] = a;
-        ran[1] = b;
-        ran[2] = c;
-        ran[3] = d;
+package day0611;
+import java.util.Scanner;
+
+public class Dice {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int ran[] = new int[4];			
+		
+		for(int i = 0; i < 4; i++) {
+			ran[i] = sc.nextInt();
+		}
+		
 		int ranCheck[] = {0, 0, 0, 0};
 		int ranCheckSum = 0;
+		
 		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < 4; j++) {
 				if(ran[i] == ran[j] && i != j) {
@@ -15,20 +23,22 @@ class Solution {
 				}
 			}
 		}
+		
 		for(int i = 0; i < 4; i++) {
 			ranCheckSum += ranCheck[i];
-			
 		}
+		
 		double answer = 0;
+		
 		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < 4; j++) {
-				if(ranCheckSum == 24) answer = 1111 * ran[0];
-				else if(ranCheckSum == 12) {									
+				if(ranCheckSum == 24) answer = 1111 * ran[0];				//2222
+				else if(ranCheckSum == 12) {									//4414
 					if(ranCheck[i] > ranCheck[j]) {
 						answer = Math.pow(10 * ran[i] + ran[j], 2);						
 					}
 				}
-				else if(ranCheckSum == 8) {
+				else if(ranCheckSum == 8) {										//6336
 					if(ran[i] < ran[j]) {
 						answer = (ran[i] + ran[j]) * Math.abs(ran[i] - ran[j]);	
 					}
@@ -37,11 +47,13 @@ class Solution {
 					if(ranCheck[i] == 2) {
 						ran[i] = 1;
 					}
-					answer = ran[0] * ran[1] * ran[2] * ran[3];
+					answer = ran[0] * ran[1] * ran[2] * ran[3];						//2526
 				}
 			}
 		}
+		
 		int min = ran[0];
+		
 		if(ranCheckSum == 0) {
 			for(int i = 1; i < ran.length; i++) {
 				if(min > ran[i]) {
@@ -50,6 +62,6 @@ class Solution {
 			}
 			answer = min;
 		}
-		return (int)answer;
-    }
+		System.out.print((int)answer);
+	}
 }
