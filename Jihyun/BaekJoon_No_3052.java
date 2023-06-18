1390b0391e3686d2aba965610c51cf509e2f63cc
wlgus123 <mail30019794@gmail.com>
2023-06-19 01:59:13 +0900

    [BOJ] 나머지 / 브론즈2
    https://www.acmicpc.net/problem/3052

---
+++ Jihyun/BaekJoon_No_3052.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BaekJoon_No_3052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> a = new ArrayList<>();
+        for(int i = 0; i < 10; i++)
+            a.add(sc.nextInt() % 42);
+
+        Collections.sort(a);
+        
+        int cnt = 1;
+        int tmp = a.get(0);
+        for(int i = 0; i < 10; i++)
+            if(a.get(i) != tmp) {
+                tmp = a.get(i);
+                cnt++;
+            }
+        System.out.println(cnt);
+    }
+}
