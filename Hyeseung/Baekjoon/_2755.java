c6413da3c2c1b859d4bbdac04cd7e13d25a06322
MsEmily1020 <peace10200khs@gmail.com>
2023-07-16 22:08:37 +0900

    [Fix] 저장 후 커밋

--- Hyeseung/Baekjoon/_2755.java
+++ Hyeseung/Baekjoon/_2755.java
@@ -9,7 +9,7 @@ public class _2755 {
         int n = Integer.parseInt(br.readLine());
 
         double sum1 = 0, sum2 = 0;
-        double[] score = { 4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7 };
+        double[] score = { 4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.0 };
         String[] scoreName = "A+,A0,A-,B+,B0,B-,C+,C0,C-,D+,D0,D-,F".split(",");
 
         for (int i = 0; i < n; i++) {
