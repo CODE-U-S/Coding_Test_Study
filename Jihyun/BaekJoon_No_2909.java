c4e3e3671801660ed2f6cbc7e00657a8fcc1e79e
wlgus123 <mail30019794@gmail.com>
2023-08-15 02:03:06 +0900

    [BOJ] 캔디 구매 / 브론즈2
    https://www.acmicpc.net/problem/2909

---
+++ Jihyun/BaekJoon_No_2909.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BaekJoon_No_2909 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double c = sc.nextDouble();
+		double k = sc.nextDouble();
+		double pow = Math.pow(10, k);
+		double a = c / pow;
+		System.out.println((int)((Math.round(a)) * pow));
+	}
+}
