fbf37103662ff09a46e1bd9083b45362762eba53
MsEmily1020 <peace10200khs@gmail.com>
2024-03-10 23:25:31 +0900

    [BOJ] 스위트콘 가격 구하기 / 브론즈5
    https://www.acmicpc.net/problem/30030

---
+++ Hyeseung/Baekjoon/_30030.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _30030 {
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		System.out.println(n - n / 11);
+	}
+}
