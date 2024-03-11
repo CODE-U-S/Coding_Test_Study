3b44f093d6e8d54993db7396f5c30406b26142d0
Jinseon <128007743+dlwlstjs@users.noreply.github.com>
2024-03-11 23:49:20 +0900

    런타임 에러 고침

--- Jinseon/color_count.java
+++ Jinseon/color_count.java
@@ -1,11 +1,15 @@
 class Solution {
     public int solution(String[][] board, int h, int w) {
         int answer = 0, n = board.length;
-        
-        if(n > h && board[h][w].equals(board[h+1][w])) answer++;
-        if(n > w && board[h][w].equals(board[h][w+1])) answer++;
-        if(h > 0 && board[h][w].equals(board[h-1][w])) answer++;
-        if(w > 0 && board[h][w].equals(board[h][w-1])) answer++;
+        String color = board[h][w];
+        int[][] save_xy = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+        for(int i = 0; i < 4; i++){
+            int x = h + save_xy[i][0];
+            int y = w + save_xy[i][1];
+            if(x >= 0 && x < n && y >= 0 && y < n){
+                if(color.equals(board[x][y])) answer++;
+            }
+        }
         return answer;
     }
 }
