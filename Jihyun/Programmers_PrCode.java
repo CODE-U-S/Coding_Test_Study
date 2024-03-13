52fdf94609a55b15fb6ace913c951b7f43b52970
wlgus123 <mail30019794@gmail.com>
2024-03-13 21:52:39 +0900

    [PGS] pr code / Level.0

---
+++ Jihyun/Programmers_PrCode.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        for(int i = r; i < code.length(); i += q)
+            answer += code.charAt(i);
+        return answer;
+    }
+}
+
+public class Programmers_PrCode {
+    public static void main(String args[]) {
+        Solution s = new Solution();
+        System.out.println(s.solution(3, 1, "qjnwezgrpirldywt"));
+    }
+}
