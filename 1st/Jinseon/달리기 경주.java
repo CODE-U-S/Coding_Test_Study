e4ea8ecc531dafa105e868ca6b03c49cb612c3c0
dlwlstjs <dlwlstjs0413@naver.com>
2024-04-01 22:44:51 +0900

    [PGS] 달리기 경주 / Level 1
    https://school.programmers.co.kr/learn/courses/30/lessons/178871

--- 1st/Jinseon/달리기 경주.java
+++ 1st/Jinseon/달리기 경주.java
@@ -1,20 +1,22 @@
+import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = new String[players.length];
-        for(int i =0 ; i < players.length; i++){
-            answer[i] = players[i];
+        String[] answer = players;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for(int i=0;i< players.length;i++)
+            map.put(players[i],i);
+
+        for(String calling : callings)
+        {
+            int count = map.get(calling);
+            String temp = answer[count-1];
+            answer[count-1] = calling;
+            answer[count] = temp;
+            map.put(calling,count-1);
+            map.put(temp,count);
         }
-        for(int i =0 ; i < callings.length; i++){
-            for(int j =0; j < players.length; j++){
-                if(callings[i].equals(answer[j])){
-                    String temp = answer[j];
-                    answer[j] = answer[j-1];
-                    answer[j-1] = temp;
-                    break;
-                }
-                
-            }
-        }
+
         return answer;
     }
 }
