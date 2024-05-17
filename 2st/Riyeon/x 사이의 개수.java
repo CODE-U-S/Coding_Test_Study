b9307914a704a8bf9e623e31723cdba0a93fe558
fldus <kry59965@gmail.com>
2024-05-17 23:04:46 +0900

    [PGS] x 사이의 개수 / Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181867

--- 2st/Riyeon/x 사이의 개수.java
+++ 2st/Riyeon/x 사이의 개수.java
@@ -4,7 +4,7 @@ class Solution {
         List<Integer> li = new ArrayList<>();
         for(String s : myString.split("x")){
             li.add(s.length());
-        }
+        } 
         if(myString.charAt(myString.length()-1) == 'x')
             li.add(0);
         return li.stream().mapToInt(i->i).toArray();
