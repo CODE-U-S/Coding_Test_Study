dcd77311ddbb60cce9d9a0b788d831d0bc730c2a
Chaeyeonwoo <codusdn0210@gmail.com>
2024-08-08 16:31:56 +0900

    [PGS] 1로 만들기 / Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181880

---
+++ 2st/Yeonwoo/1로 만들기.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = 0
+    for n in num_list:
+        while n!=1:
+            if n%2==0:
+                n = n//2
+            else:
+                n = (n-1)//2
+            answer += 1
+    return answer
