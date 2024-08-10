add43645f48cce71fa38820673a3b4fe748f5616
Chaeyeonwoo <codusdn0210@gmail.com>
2024-08-10 14:28:06 +0900

    [PGS] PCCE 기출문제 가채점 / Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/250128

---
+++ 2st/Yeonwoo/PCCE 기출문제 가채점.py
@@ -0,0 +1,9 @@
+def solution(numbers, our_score, score_list):
+    answer = []
+    for i in range(len(numbers)):
+        if our_score[i] == score_list[numbers[i]-1]:
+            answer.append("Same")
+        else:
+            answer.append("Different")
+    
+    return answer
