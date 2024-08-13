c1830425bfdb7b4dd523ad865dcbd0002f022415
Chaeyeonwoo <codusdn0210@gmail.com>
2024-08-13 14:06:01 +0900

    [PGS] 배열 만들기 / Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181921

---
+++ 2st/Yeonwoo/배열 만들기 2.py
@@ -0,0 +1,9 @@
+def solution(l, r):
+    answer = []
+    temp = [-1]
+    for i in range(l,r+1):
+        if str(i).count('0') + str(i).count('5') == len(str(i)):
+            answer.append(i)
+    if answer == []:
+        answer.append(-1)
+    return answer
