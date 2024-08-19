5f01c55af4686e85b3fed9842826eac85a4e5207
jaypark <soRjdla0709@gmail.com>
2024-08-19 17:06:52 +0900

    2024.08.19 commit

--- 3st/Jaeyoung/피타고라스의 정의.java
+++ 3st/Jaeyoung/피타고라스의 정의.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
