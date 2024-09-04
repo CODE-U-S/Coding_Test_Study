2061204561c17b160560f8296a65194254fbc61b
juio12 <gtase08_@naver.com>
2024-09-04 19:29:36 +0900

    [PGS] 중앙값 구하기 / Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/120811

---
+++ 3st/Juhee/중앙값 구하기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Arrays.sort(array);
+        answer = array[array.length/2];
+        return answer;
+    }
+}
