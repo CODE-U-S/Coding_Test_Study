7ce9af688bd8fa0f671eddaaa6bb5f60f2c0914e
jaypark <soRjdla0709@gmail.com>
2024-09-15 22:28:57 +0900

    [PGS] 점의 위치 구하기 / Level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/120841

--- 3st/Jaeyoung/점의 위치 구하기.java
+++ 3st/Jaeyoung/점의 위치 구하기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] dot) {
         int answer = 0;
-        if(dot[0]>0 && dot[1]>0) answer=1;
+        if(dot[0]>0 && dot[1]>0) answer =1;
         else if(dot[0]<0 && dot[1]>0) answer=2;
         else if(dot[0]>0 && dot[1]<0) answer=4;
         else if(dot[0]<0 && dot[1]<0) answer=3;
