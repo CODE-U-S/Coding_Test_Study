2842d380cc3ad87deb0a4f7c0b397e7bc65bf636
jeahee2 <leewpgml@gmail.com>
2024-09-15 22:42:39 +0900

    [PGS] 숫자 비교하기/Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/120807

---
+++ 3st/Jaehee/숫자 비교하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        if(num1 == num2){
+        int answer = 1;
+        return answer;
+    }
+    else{
+        int answer = -1;
+        return answer;
+        }
+    }
+}
