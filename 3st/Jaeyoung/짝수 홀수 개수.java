e208f48718d27ae48eea32a6c4d831795946f5d4
jaypark <soRjdla0709@gmail.com>
2024-09-18 23:25:46 +0900

    [PGS] 짝수 홀수 개수 / Level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/120824

---
+++ 3st/Jaeyoung/짝수 홀수 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        for(int i =0;i<num_list.length;i++){
+            if(num_list[i]%2==0){
+                answer[0]++;
+            }
+            else if(num_list[i]%2==1){
+                answer[1]++;
+            }
+            else if(num_list[i]%2==-1){
+                answer[1]++;
+            }
+        }
+        return answer;
+    }
+}
