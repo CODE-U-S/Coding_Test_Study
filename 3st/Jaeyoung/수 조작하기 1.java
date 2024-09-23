cd5b23cd063411c8ca6df1663efdfd15ef1d4ecb
jaypark <soRjdla0709@gmail.com>
2024-09-23 23:37:16 +0900

    [PGS] 수 조작하기 / Level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181926

---
+++ 3st/Jaeyoung/수 조작하기 1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        for(int i=0; i<control.length(); i++)
+            switch(control.charAt(i)){
+                case 'w': 
+                    n+=1;
+                    break;
+                case 's': 
+                    n-=1;
+                    break;
+                case 'd': 
+                    n+=10;
+                    break;
+                case 'a': 
+                    n-=10;
+                    break;
+            }
+        return n;
+    }
+}
