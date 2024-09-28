8e33d34df613490ca9dbfadda16edd2d927f4420
hyeonseo8822 <hyeonseojo929@gmail.com>
2024-09-28 17:15:15 +0900

    [PGS]문자열 뒤집기/ level 0
    https://school.programmers.co.kr/learn/courses/30/lessons/120822

--- 3st/Hyeonseo/programmer/하드샤 수.java
+++ 3st/Hyeonseo/programmer/하드샤 수.java
@@ -11,7 +11,3 @@ class Solution {
         return answer;
     }
 }
-//level 1
-//https://school.programmers.co.kr/learn/courses/30/lessons/12947
-//양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 예를 들어 18의 자릿수 합은 1+8=9이고, 
-//18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
