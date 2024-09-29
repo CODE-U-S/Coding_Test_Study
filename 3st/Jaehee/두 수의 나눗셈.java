277968ad83b42083f80c7f4e7ccf02b17fedf146
jeahee2 <leewpgml@gmail.com>
2024-09-29 20:08:00 +0900

    [PGS] 두 수의 나눗셈/Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/120806

--- 3st/Jaehee/두 수의 나눗셈.java
+++ 3st/Jaehee/두 수의 나눗셈.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1/num2;
-        return answer;
+        return (int)((double)num1 / num2 * 1000);
+ 
     }
 }
