077c2764db49472210184a86d3bf972e5d626a98
juio12 <gtase08_@naver.com>
2024-10-03 23:52:09 +0900

    [PGS] 가까운 1 찾기 / Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181898

--- 3st/Juhee/카운트 다운.java
+++ 3st/Juhee/카운트 다운.java
@@ -1,8 +1,8 @@
 class Solution {
-    public int[] solution(int start, int end) {
-        int[] answer = new int[start-end+1];
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[start_num-end_num+1];
         for(int i=0; i<answer.length ; i++){
-            answer[i] = start--;
+            answer[i] = start_num--;
         }
         return answer;
     }
