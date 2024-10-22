357a63f3dc03742798f855c9f821a99b0b92a32f
hyeonseo8822 <hyeonseojo929@gmail.com>
2024-10-22 23:51:47 +0900

    [PGS] 자릿수 더하기 / level 1
    https://school.programmers.co.kr/learn/courses/30/lessons/12931

--- 3st/Hyeonseo/programmer/양꼬치.java
+++ 3st/Hyeonseo/programmer/양꼬치.java
@@ -12,5 +12,4 @@ class Solution {
     }
 }
 //level 0 
-//https://school.programmers.co.kr/learn/courses/30/lessons/120830
 // 머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다. 양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다. 정수 n과 k가 매개변수로 주어졌을 때, 양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.
