316f4150e69819ef7958bdde8c9d1bbe2b4d9ecd
hyeonseo8822 <hyeonseojo929@gmail.com>
2024-10-25 12:11:28 +0900

    [PGS] 정수 내름차순으로 배치하기 / level 1
    https://school.programmers.co.kr/learn/courses/30/lessons/12933

--- 3st/Hyeonseo/programmer/중앙값 구하기.java
+++ 3st/Hyeonseo/programmer/중앙값 구하기.java
@@ -8,6 +8,4 @@ class Solution {
         return n;
     }
 }
-//https://school.programmers.co.kr/learn/courses/30/lessons/120811
-//중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
 //level 0
