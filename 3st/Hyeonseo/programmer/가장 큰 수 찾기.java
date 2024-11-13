b210ef063a5bf73b347f7515a79445f2f415596c
hyeonseo8822 <hyeonseojo929@gmail.com>
2024-11-13 18:02:34 +0900

    [PGS]가장 큰수 / level 2
    https://school.programmers.co.kr/learn/courses/30/lessons/42746

--- 3st/Hyeonseo/programmer/가장 큰 수 찾기.java
+++ 3st/Hyeonseo/programmer/가장 큰 수 찾기.java
@@ -17,5 +17,3 @@ class Solution {
     }
 }
 //level 0
-//https://school.programmers.co.kr/learn/courses/30/lessons/120899
-//  정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
