2bca4f899076c4ebed83651c50370315853c1166
hyeonseo8822 <hyeonseojo929@gmail.com>
2024-11-15 23:44:05 +0900

    [PGS] 배열 뒤집기 / level 0
    https://school.programmers.co.kr/learn/courses/30/lessons/120821

--- 3st/Hyeonseo/programmer/약수의 개수와 덧셈.java
+++ 3st/Hyeonseo/programmer/약수의 개수와 덧셈.java
@@ -18,5 +18,3 @@ class Solution {
     }
 }
 //level 1
-// https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=java
-//두 정수 left와 right가 매개변수로 주어집니다. left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
