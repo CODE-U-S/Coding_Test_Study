67ed3d645ca50ea6a42692fc1323d35a1a606673
fldus <kry59965@gmail.com>
2024-11-20 01:06:21 +0900

    [PGS] 조건에 맞는 개발자 찾기 / Lv.2
    https://school.programmers.co.kr/learn/courses/30/lessons/276034

---
+++ 2st/Riyeon/조건에 맞는 개발자 찾기.SQL
@@ -0,0 +1,9 @@
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE SKILL_CODE & (SELECT CODE
+                     FROM SKILLCODES
+                     WHERE NAME = 'Python')
+    OR SKILL_CODE & (SELECT CODE
+                     FROM SKILLCODES
+                     WHERE NAME = 'C#')
+ORDER BY ID
