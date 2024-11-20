12cfaf7e34a7d82bbb1cdabe8005bd50a04fe46b
jaypark <soRjdla0709@gmail.com>
2024-11-20 23:53:23 +0900

    [PGS] 문자열의 앞의 n글자 / Level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181907

---
+++ 3st/Jaeyoung/문자열의 앞의 n글자.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        
+        answer=my_string.substring(0,n);
+        
+        return answer;
+    }
+}
