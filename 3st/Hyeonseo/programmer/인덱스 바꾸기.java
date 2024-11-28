b3de09a68346c6297cf11745982716bd31aa7cb0
hyeonseo8822 <hyeonseojo929@gmail.com>
2024-11-28 15:17:52 +0900

    [PGS] 문자열 붙여서 출력하기 / level 0
    https://school.programmers.co.kr/learn/courses/30/lessons/181946

--- 3st/Hyeonseo/programmer/인덱스 바꾸기.java
+++ 3st/Hyeonseo/programmer/인덱스 바꾸기.java
@@ -10,6 +10,4 @@ class Solution {
         return answer;
     }
 }
-//httpst://school.programmers.co.kr/learn/courses/30/lessons/120895
-//문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
 //level  0
