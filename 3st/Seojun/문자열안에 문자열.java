62c77ea32e199b6801e72674be66ef7c25750fe7
esna1032 <kinseoj1@gmail.com>
2024-12-02 23:02:48 +0900

    [PGS] 문자열안에 문자열/ level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/120908

---
+++ 3st/Seojun/문자열안에 문자열.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String str1, String str2) {
+      
+        if (str1.contains(str2)) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
