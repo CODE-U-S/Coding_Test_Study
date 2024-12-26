1a9f04c142fa1c40e2cdca4fc97ee466304efdd0
juio12 <gtase08_@naver.com>
2024-12-26 22:23:17 +0900

    [PGS] 이진수 더하기 / Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/120885

---
+++ 3st/Juhee/이진수 더하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String bin1, String bin2) {
+        String answer = "";
+        answer = Integer.toBinaryString(Integer.parseInt(bin1,2) + Integer.parseInt(bin2,2));
+        return answer;
+    }
+}
