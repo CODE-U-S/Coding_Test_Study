9c7e58f9033fcf93d00bb381bd68f02f931c6f70
jeahee2 <leewpgml@gmail.com>
2024-12-28 22:38:59 +0900

    [PGS] 알람 시계/Lv.0
    https://www.acmicpc.net/submit/2884/87917340

---
+++ 3st/Jaehee/알람 시계.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+		
+		int H = in.nextInt(); // 시
+		int M = in.nextInt(); // 분
+		in.close();
+		
+		if(M < 45) {
+			H--;
+			M= 60 - (45 - M); 	
+			if(H < 0) {
+				H = 23;
+			}
+			System.out.println(H + " " + M);
+		}
+		else {
+			System.out.println(H + " " + (M - 45));
+		}
+	}
+}
