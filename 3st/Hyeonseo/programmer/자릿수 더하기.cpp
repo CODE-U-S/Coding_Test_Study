e4a3aae97f195975b9fec24dd299c97afcaa0e47
hyeonseo8822 <hyeonseojo929@gmail.com>
2025-01-22 23:56:05 +0900

    [PGS] 자릿수 더하기 / level 1
    https://school.programmers.co.kr/learn/courses/30/lessons/12931?language=cpp

---
+++ 3st/Hyeonseo/programmer/자릿수 더하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+    string str = to_string(n);
+    for (char c : str)
+    {
+        answer += c - '0';
+    }
+
+    return answer;
+}
