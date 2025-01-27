4eff815e1f5f5153faa4c4c79f963230149f0143
fldus <kry59965@gmail.com>
2025-01-28 01:39:35 +0900

    [BOJ] 문자와 문자열 / 브론즈5
    https://www.acmicpc.net/problem/27866

---
+++ 2st/Riyeon/문자와 문자열.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int idx = sc.nextInt();
+		System.out.println(str.charAt(idx-1));
+	}
+}
