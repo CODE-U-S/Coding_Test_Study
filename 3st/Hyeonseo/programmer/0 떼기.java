8d4c0a6f8aab6b65397fb1c196c16943f8daf792
hyeonseo8822 <hyeonseojo929@gmail.com>
2025-03-13 22:08:13 +0900

    [PGS] 점의 위치 구하기 / level 0
    https://school.programmers.co.kr/learn/courses/30/lessons/120841

---
+++ 3st/Hyeonseo/programmer/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int a = Integer.parseInt(n_str);
+        answer = String.valueOf(a);
+        return answer;
+    }
+}
