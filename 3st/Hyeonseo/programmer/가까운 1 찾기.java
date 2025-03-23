0d98ecf5cae4ef8ef93945bfb66e7deeab53c020
hyeonseo8822 <hyeonseojo929@gmail.com>
2025-03-23 16:08:09 +0900

    [d]

---
+++ 3st/Hyeonseo/programmer/가까운 1 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = -1;
+        for(int i = idx; i < arr.length; i++) {
+            if(arr[i] == 1) {
+                return i;            }
+        }
+        return answer;
+    }
+}
