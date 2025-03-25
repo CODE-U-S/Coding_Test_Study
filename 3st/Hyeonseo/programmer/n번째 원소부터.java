dbe5572500bb06f179f8b7013bc18ef42ba09308
hyeonseo8822 <hyeonseojo929@gmail.com>
2025-03-25 18:51:24 +0900

    4st

---
+++ 3st/Hyeonseo/programmer/n번째 원소부터.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length - n + 1];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = num_list[n-1];
+            n++;
+        }
+        return answer;
+    }
+}
