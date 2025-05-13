eb978a9c90a83cc238682c055133a385f73a633b
unknown <choihanni27@gmail.com>
2025-05-13 15:57:41 +0900

    [BOJ] 꼬마 정민 / 브론즈5
    https://www.acmicpc.net/problem/11382

--- 4st/Hani/baekjoon/꼬마 정민.java
+++ 4st/Hani/baekjoon/꼬마 정민.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        System.out.println((A + B));
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        System.out.println(A + B + C);
     }
-  
+}
