c746fbc5e4629935b65d5c54c1e313f2b758098b
limjisoo <s2552@e-mirim.hs.kr>
2025-05-15 20:54:53 +0900

    [BOJ] 꼬마정민 / 브론즈5
    https://www.acmicpc.net/problem/11382

---
+++ 4st/Jisoo/baekjoon/A/B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+    Scanner scan = new Scanner(System.in);
+        double A= scan.nextInt();
+        double B= scan.nextInt();
+        System.out.print(A/B);
+    }
+}
