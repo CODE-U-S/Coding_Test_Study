4def7cf97d207e657cd5036397bc0612bf893d72
이메이덩 <ha0402junglim@gmail.com>
2025-05-29 19:18:13 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    if(n >= 1 && n <= 100) {
+            if(n % 7 == 0) {
+                answer = n / 7;
+            } else {
+                answer = n / 7 + 1;
+            }
+        }
+    return answer;
+}
