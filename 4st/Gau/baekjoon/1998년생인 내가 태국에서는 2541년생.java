cd823f82ac117df1b22f36c4ef598daf8cdc8356
1304-lukeyu <yoonleena0313@gmail.com>
2025-05-29 09:22:47 +0900

    [BOJ] 16일차/ 노가우 / 1998년생인 내가 태국에서는 2541년생?! / 브론즈 5
    https://www.acmicpc.net/submit/18108/94860269

--- 4st/Gau/baekjoon/1998년생인 내가 태국에서는 2541년생.java
+++ 4st/Gau/baekjoon/1998년생인 내가 태국에서는 2541년생.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        while (sc.hasNext()) {
-	        int a=sc.nextInt();
-       		int b=sc.nextInt();
-            System.out.println(a + b);
-        }
+        int a=sc.nextInt();
+        int b=a-543;
+        System.out.println(b);
     }
 }
