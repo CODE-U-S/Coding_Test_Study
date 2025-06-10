f4ec62cc2d97a3d3e7689c85e147ae6d127166d8
unknown <choihanni27@gmail.com>
2025-06-10 21:25:25 +0900

    [BOJ] A+B-8 / 브론즈5
    https://www.acmicpc.net/problem/11022

---
+++ 4st/Hani/baekjoon/A+B-8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int sum[] = new int[t];
+        int num1[] = new int[t];
+        int num2[] = new int[t];
+        
+        for (int i = 0; i < t; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            sum[i] = A + B;
+            num1[i] = A;
+            num2[i] = B;
+        }
+        for(int i = 0; i < t; i++)
+            System.out.println("Case #" + (i+1) + ": " + num1[i] + " + " + num2[i] + " = " + sum[i]);
+    }
+}
