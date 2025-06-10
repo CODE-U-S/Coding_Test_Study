b2acb9f70bd328c95417c4d760f5288cc2e6ee18
gkwydl <ha0402junglim@gmail.com>
2025-06-10 20:12:04 +0900

    [PGS] 최댓값 만들기(1) / level 0
    https://school.programmers.co.kr/learn/courses/30/lessons/120847

---
+++ 3st/Hajeong/programmer/최댓값 만들기(1).java
@@ -0,0 +1,8 @@
+for(int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] * numbers[j] > max) {
+                    max = numbers[i] * numbers[j];
+                }
+            }
+        }
+        answer = max;
