38d1c251179163de1b889d56bd8bd29b6d988a3b
gushgus <s2547@e-mirim.hs.kr>
2025-06-17 00:11:39 +0900

    [PGS] 짝수 홀수 개수 / Level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/120824

---
+++ 4st/Seohyun/programmer/짝수 홀수 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        
+        for(int i=0; i<num_list.length; i++) {
+            if(num_list[i]%2==0){
+                answer[0]++;
+            }
+            else if(num_list[i]%2 !=0) {
+                answer[1]++;
+            }
+        }
+        return answer;
+    }
+}
