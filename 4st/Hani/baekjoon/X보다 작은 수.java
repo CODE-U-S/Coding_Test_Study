c21498dba20f4d260d2b7efe573b3973a2d19c72
unknown <choihanni27@gmail.com>
2025-06-19 08:19:39 +0900

    [BOJ] 개수 세기/ 브론즈5
    https://www.acmicpc.net/problem/10807

---
+++ 4st/Hani/baekjoon/X보다 작은 수.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            if (a[i] < x)
+                System.out.print(a[i] + " ");
+        }
+    }
+}
