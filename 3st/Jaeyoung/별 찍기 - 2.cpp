972d0f31abe9070b391f41c2d615a26a0bf08cde
jaypark <soRjdla0709@gmail.com>
2025-06-27 10:35:45 +0900

    [BOJ] 별 찍기 - 2 / Bronze 4
    https://www.acmicpc.net/problem/2439

---
+++ 3st/Jaeyoung/별 찍기 - 2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num;
+    scanf("%d", &num);
+
+    for( int i = 1; i <= num; i++ ) {
+        for (int j = 1; j <= num - i; j++) {
+            printf(" ");
+        }
+        for( int j = 1; j <= i; j++ ) {
+            printf( "*" );
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
