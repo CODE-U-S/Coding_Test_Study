af0dc4041194a8bc8aaaa64c88827a6d430f17e9
choihanni <choihanni27@gmail.com>
2025-07-18 13:00:03 +0900

    [PGS] 머쓱이보다 키 큰 사람 / level0
    https://school.programmers.co.kr/learn/courses/30/lessons/120585

---
+++ codingTest2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class codingTest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A, B = sc.nextInt();
+		System.out.println("A : ");
+		System.out.println("B : ");
+		System.out.print("A + B : ");
+		System.out.print(A + B);
+	}
+}
