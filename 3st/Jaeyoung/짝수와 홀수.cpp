6953c3244ac68712e3fe6a8580a751a4c51ceb7a
parkjay0709 <soRjdla0709@gmail.com>
2025-07-24 10:19:03 +0900

    [BOJ] Hook / Bronze 5
    https://www.acmicpc.net/problem/10189

---
+++ 3st/Jaeyoung/짝수와 홀수.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution( int num )
+{
+    string answer = "";
+    if (num % 2 == 0)
+        answer = "Even";
+    else
+        answer = "Odd";
+    return answer;
+}
