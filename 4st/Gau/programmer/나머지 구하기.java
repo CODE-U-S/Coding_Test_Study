daccbe1f65b2ec89357e6d258f358686eeb6b336
1304-lukeyu <yoonleena0313@gmail.com>
2025-08-13 21:28:11 +0900

    [PGS] 노가우 / 96일차 / 문자열 붙여서 출력하기
    https://school.programmers.co.kr/learn/courses/30/lessons/181946#qna

---
+++ 4st/Gau/programmer/나머지 구하기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Solution {
+    public int solution(int num1, int num2) {
+        Scanner sc=new Scanner(System.in);
+        int answer = -1;
+        return num1%num2;
+    }
+}
