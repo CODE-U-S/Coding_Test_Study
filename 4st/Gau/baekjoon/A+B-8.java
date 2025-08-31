fb69b2e814208903748212fc9b735297b6c0118d
1304-lukeyu <yoonleena0313@gmail.com>
2025-08-31 22:36:33 +0900

    [PGS] 노가우 / 114일차 / 두 수의 연산값 비교하기
    https://school.programmers.co.kr/learn/courses/30/lessons/181938

--- 4st/Gau/baekjoon/A+B-8.java
+++ 4st/Gau/baekjoon/A+B-8.java
@@ -1,13 +1,17 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num=sc.nextInt();
+        int T = sc.nextInt(); 
 
-        for(int i=0; i<num; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(a+b);
+        for (int i = 1; i <= T; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int sum = A + B;
+            System.out.printf("Case #%d: %d + %d = %d\n", i, A, B, sum);
         }
+
+        sc.close();
     }
 }
