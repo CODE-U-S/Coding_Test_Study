a315e1ec286e661d6b1b7959cef8edfe1e76c1d2
gushgus <s2547@e-mirim.hs.kr>
2025-09-08 23:40:30 +0900

    [PGS] 중앙값 구하기 / Level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/120811

--- 4st/Jisoo/programmer/중앙값 구하기.java
+++ 4st/Jisoo/programmer/중앙값 구하기.java
@@ -1,10 +1,8 @@
 import java.util.Arrays; 
 class Solution {
     public int solution(int[] array) {
-        int answer = 0;
         Arrays.sort(array);
-        int mid=array.length/2;
-        answer=array[mid];
-        return answer;
+        int mid = array.length / 2;
+        return array[mid];
     }
 }
