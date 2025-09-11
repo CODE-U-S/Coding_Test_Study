b5a903f2c7799ae841185eb30bd4ae579671ac89
조현서 <hyeonseojo929@gmail.com>
2025-09-11 13:25:01 +0900

    S

--- 4st/Seohyun/programmer/양꼬치.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int n, int k) {
-    int answer = n*12000+k*2000-(n/10)*2000;
-    return answer;
-}
--- 4st/Seohyun/programmer/양꼬치.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int solution(int n, int k) {
-        int answer=0;
-        answer=n*12000+k*2000;
-        if(n>=10)
-            answer=answer-(n/10)*2000;
-        return answer;
-    }
-}
