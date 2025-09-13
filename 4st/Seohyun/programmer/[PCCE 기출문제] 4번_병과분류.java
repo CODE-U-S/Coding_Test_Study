558b04fe772069ac17b338fb3b90cb54eb831af5
parkjay0709 <soRjdla0709@gmail.com>
2025-09-13 11:58:17 +0900

    b

---
+++ 4st/Seohyun/programmer/[PCCE 기출문제] 4번_병과분류.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String code = sc.next();
+        String lastFourWords = code.substring(code.length()-4, code.length());
+
+        if(lastFourWords.equals("_eye")){
+            System.out.println("Ophthalmologyc");
+        }
+        else if(lastFourWords.equals("head")){
+            System.out.println("Neurosurgery");
+        }
+        else if(lastFourWords.equals("infl")){
+            System.out.println("Orthopedics");
+        }
+        else if(lastFourWords.equals("skin")){
+            System.out.println("Dermatology");
+        }
+        else
+{
+            System.out.println("direct recommendation");
+        }
+    }
+}
