27b479140f558e359a86fb583d76bec25ff9d29f
parkjay0709 <soRjdla0709@gmail.com>
2025-09-15 09:19:39 +0900

    [BOJ] 연세대학교 / Bronze 5
    https://www.acmicpc.net/problem/15680

---
+++ 3st/Jaeyoung/연세대학교.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if( n )
+        cout << "Leading the Way to the Future";
+    else 
+        cout << "YONSEI";
+    
+    return 0;
+}
