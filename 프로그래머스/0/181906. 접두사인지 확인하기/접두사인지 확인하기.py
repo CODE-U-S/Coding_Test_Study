56d666f88624339f952768af01a401512c8fc21e
조현서 <hyeonseojo929@gmail.com>
2025-09-20 19:11:53 +0900

    S

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++
@@ -1,2 +0,0 @@
-def solution(my_string, is_prefix):
-    return 1 if my_string.startswith(is_prefix) else 0
--- 프로그래머스/1/151136. 평균 일일 대여 요금 구하기/평균 일일 대여 요금 구하기.sql
+++
@@ -1,3 +0,0 @@
-select round(avg(DAILY_FEE), 0) as AVERAGE_FEE
-from CAR_RENTAL_COMPANY_CAR
-where CAR_TYPE = 'SUV';
