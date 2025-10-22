bf037085eebe7ef076fbc411e2017f86844d4b96
공은성 <adfvfe9@gmail.com>
2025-10-22 13:34:23 +0900

    [PGS] 왼쪽 오른쪽 / level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181890

---
+++ 4st/Eunseong/왼쪽 오른쪽.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] str_list) {
+        String no[] = {};
+        for (int i = 0; i < str_list.length; i++) {
+            if (str_list[i].equals("l")) {
+                return Arrays.copyOfRange(str_list, 0, i);
+            }
+            if (str_list[i].equals("r")) {
+                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
+            }
+        }
+        return no;
+    }
+}
