342cf761f7d8bfe7990e6a61042a5f3fd350d3c9
공은성 <adfvfe9@gmail.com>
2025-10-28 18:58:55 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/181851#

---
+++ 4st/Eunseong/전국 대회 선발 고사.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        int top3[] = new int[3];
+        int cnt = 0;
+        
+        for (int i = 0; i < rank.length; i++) {
+            for (int j = 0; j < rank.length; j++) {
+                if (rank[j] == i + 1) {
+                    if (attendance[j]) {
+                        top3[cnt++] = j;
+                    }
+                    break;
+                }
+            }
+            if (cnt == 3) {
+                break;
+            }
+        }
+        
+        return 10000 * top3[0] + 100 * top3[1] + top3[2];
+    }
+}
