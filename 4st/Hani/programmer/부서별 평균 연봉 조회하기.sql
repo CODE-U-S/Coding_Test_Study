f7ff68d7f4fdffcb150f553d72bd81b5397ce1da
한이 <choihanni27@gmail.com>
2025-11-07 23:48:31 +0900

    [PGS] 부서별 평균 연봉 조회하기 / level3
    https://school.programmers.co.kr/learn/courses/30/lessons/284529

--- 4st/Hani/programmer/부서별 평균 연봉 조회하기.sql
+++
@@ -1,6 +0,0 @@
-select D.DEPT_ID, D.DEPT_NAME_EN, round(avg(E.SAL), 0) as AVG_SAL 
-from HR_DEPARTMENT D
-join HR_EMPLOYEES E
-on D.DEPT_ID = E.DEPT_ID
-group by D.DEPT_ID, D.DEPT_NAME_KR
-order by AVG_SAL desc;
