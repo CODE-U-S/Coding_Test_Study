acbba3e93134e5eb575ed7276a7e7821f1737b9e
한이 <choihanni27@gmail.com>
2025-12-26 20:21:16 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 1.12 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        
+        int[] answer = new int[num_list.length - 5];
+        
+        for (int i = 5; i < num_list.length; i++) {
+            answer[i - 5] = num_list[i];
+        }
+        return answer;
+    }
+}
