fc7af6cf2e148a3a9fb62eb67d2f384885af2354
공은성 <adfvfe9@gmail.com>
2026-01-02 21:11:01 +0900

    [BOJ] A / B / 브론즈 5
    https://www.acmicpc.net/problem/1008

---
+++ 4st/Eunseong/A 나누기 B.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.print((double)s.nextInt() / s.nextInt());
+    }
+}
