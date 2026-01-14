34917fcfd1a9518cc523ad5aae6b4d6b3aa5ba24
공은성 <adfvfe9@gmail.com>
2026-01-14 23:12:01 +0900

    [BOJ] A + B (2) / 브론즈 5
    https://www.acmicpc.net/problem/2558

---
+++ 4st/Eunseong/A+B(2).java
@@ -0,0 +1,8 @@
+public import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextInt() + s.nextInt());
+    }
+}
