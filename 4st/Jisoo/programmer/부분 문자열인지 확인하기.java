ad554761c588a917ce08a18cd4fe09efeeb0524c
limjisoo <s2552@e-mirim.hs.kr>
2026-01-19 22:35:54 +0900

    [PGS] 문자열을 정수로 변환하기 / Level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181848

---
+++ 4st/Jisoo/programmer/부분 문자열인지 확인하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        if (my_string.contains(target)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
