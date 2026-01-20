9fb48c34a2a81f75fd8d6dabcfba8f03357fdbf4
1304-lukeyu <yoonleena0313@gmail.com>
2026-01-21 08:52:32 +0900

    [257일차] 노가우 / 문자열출력하기 / Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181952?language=java

--- 4st/Gau/programmer/몫 구하기.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-class Solution {
-    public int solution(int num1, int num2) {
-        Scanner sc=new Scanner(System.in);
-        return num1/num2;
-    }
-}
--- 4st/Gau/programmer/특수문자 출력하기.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class Solution {
-    public static void main(String[] args) {
-      System.out.print("!@#$%^&*(\\'\"<>?:;"); 
-    }
-}
