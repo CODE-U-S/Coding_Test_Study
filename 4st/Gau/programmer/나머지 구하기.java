6f3c9e70e74ad59f27bb8bde2edd5447ccb94fd7
1304-lukeyu <yoonleena0313@gmail.com>
2026-01-21 08:52:32 +0900

    [257일차] 노가우 / 문자열출력하기 / Lv.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181952?language=java

--- 4st/Gau/programmer/나머지 구하기.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-class Solution {
-    public int solution(int num1, int num2) {
-        Scanner sc=new Scanner(System.in);
-        int answer = -1;
-        return num1%num2;
-    }
-}
