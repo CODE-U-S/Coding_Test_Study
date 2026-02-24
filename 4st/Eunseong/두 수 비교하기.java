381701f1d8e8605e11698561f9ba2553683d163f
공은성 <adfvfe9@gmail.com>
2026-02-24 18:12:34 +0900

    [BOJ] 두 수 비교하기 / 브론즈 V
    https://www.acmicpc.net/problem/1330

---
+++ 4st/Eunseong/두 수 비교하기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        if (a > b) {
+            System.out.println(">");
+        } else if (a == b) {
+            System.out.println("==");
+        } else {
+            System.out.println("<");
+        }
+    }
+}
