20002747c1ccbb7d9f6680bb245c3804b7635200
choihanni27 <choihanni27@gmail.com>
2026-03-18 22:08:27 +0900

    [PGS] 날짜 비교하기 / Level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/181838

---
+++ 4st/Jisoo/programmer/날짜 비교하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        if (date1[0] < date2[0]) return 1;
+        if (date1[0] > date2[0]) return 0;
+
+        if (date1[1] < date2[1]) return 1;
+        if (date1[1] > date2[1]) return 0;
+
+        if (date1[2] < date2[2]) return 1;
+        return 0;
+    }
+}
