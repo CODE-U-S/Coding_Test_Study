d350a82d04f4cae4a96c9cac206eb3acffc34234
gy-urii <kyuri4815@gmail.com>
2026-06-22 21:20:29 +0900

    [86일차/김규리]정수 찾기
    https://school.programmers.co.kr/learn/courses/30/lessons/181840

---
+++ 5st/gyuri/정수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        
+        for(int i=0; i < num_list.length; i++){
+            if(num_list[i] == n){
+                answer = 1;
+                break;
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
