3e103ac291a2c125fcf36aa36d6bdf8f99525766
한이 <choihanni27@gmail.com>
2026-06-29 15:59:16 +0900

    [PGS] 문자열 정수의 합 / level0
    https://school.programmers.co.kr/learn/courses/30/lessons/181849?language=python3

---
+++ 4st/Hani/programmer/문자열 정수의 합.py
@@ -0,0 +1,9 @@
+def solution(num_str):
+    answer = 0
+    
+    num = int(num_str)  
+    
+    for i in range(len(num_str)) :
+        answer += num % 10
+        num = num // 10
+    return answer
