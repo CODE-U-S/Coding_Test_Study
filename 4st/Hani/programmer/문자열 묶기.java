df28f9f80af6d68e87e403c7d2366f41746884bb
한이 <choihanni27@gmail.com>
2026-07-08 23:22:03 +0900

    [PGS] 문자열 묶기 / level1
    https://school.programmers.co.kr/learn/courses/30/lessons/181855?language=java

---
+++ 4st/Hani/programmer/문자열 묶기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] strArr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < strArr.length; i++) {
+            int len = strArr[i].length();
+            map.put(len, map.getOrDefault(len, 0) + 1);
+        }
+        
+        int count = 0;
+        for (int value : map.values()) {
+            if (value > count) {
+                count = value;
+            }
+        }
+        
+        return count;
+    }
+}
