5ebdf782d9c057574715e78e71243021e6f55a69
gy-urii <kyuri4815@gmail.com>
2026-07-14 23:52:44 +0900

    [106일차 / 김규리] / x만큼 간격이 있는 n개의 숫자
    https://school.programmers.co.kr/learn/courses/30/lessons/12954

---
+++ 5st/gyuri/x만큼 간격이 있는 n개의 숫자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        
+        for (int i = 0; i < n; i++) {
+            answer[i] = (long) x * (i + 1);
+        }
+        return answer;
+    }
+}
