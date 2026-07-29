e21ed3c9236a9ca259ef65a1c4193d50aa9e78a3
한이 <choihanni27@gmail.com>
2026-07-29 17:34:30 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.10 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    var answer = 0;
+    
+    numbers.sort()
+    
+    for (let i = 0; i <= 9; i++) {
+        if (!numbers.includes(i)) {
+            answer += i
+        }
+    }
+    return answer;
+}
