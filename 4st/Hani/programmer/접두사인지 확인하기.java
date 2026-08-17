9c387ed4ad7f7e39d8e1a810ed739f0dadd58484
한이 <choihanni27@gmail.com>
2026-08-17 23:05:58 +0900

    [PGS] 접미사 배열 / level1
    https://school.programmers.co.kr/learn/courses/30/lessons/181909

--- 4st/Hani/programmer/접두사인지 확인하기.java
+++ 4st/Hani/programmer/접두사인지 확인하기.java
@@ -11,3 +11,22 @@ class Solution {
         return 1;
     }
 }
+
+class Solution1 {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        
+        String[] temp = new String[my_string.length()];
+        temp[0] = String.valueOf(my_string.charAt(0));
+        
+        for (int i = 1; i < temp.length; i++) {
+            temp[i] = temp[i-1] + my_string.charAt(i);
+        }
+        
+        for (int i = 1; i < temp.length; i++) {
+            if (temp[i].equals(is_prefix)) 
+                answer = 1;
+        }
+        return answer;
+    }
+}
